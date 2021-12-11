abbdc0d544acbb93243a40a19cf8e3b057bafe6d
joankirui <joankirui99@gmail.com>
2021-12-11 20:52:40 +0300

    create Profile model

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+import datetime as dt
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    prof_pic = CloudinaryField('image')
+    bio = models.CharField(max_length=30,blank=True,null=True)
+    contact = models.EmailField()
+
+    def __str__(self):
+        return f'{self.user.username}Profile'
