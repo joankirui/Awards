23eea4444603facdd172c4eef2767518f6071a43
joankirui <joankirui99@gmail.com>
2021-12-11 21:29:02 +0300

    create profiles automatically when a new user is created

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 import datetime as dt
 from django.http import Http404
 from django.core.exceptions import ObjectDoesNotExist
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -14,6 +16,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}Profile'
+
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
