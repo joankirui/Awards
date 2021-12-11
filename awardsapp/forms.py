3c512be59c3b4931d2c200592337f5c7d114a8bc
joankirui <joankirui99@gmail.com>
2021-12-11 23:35:42 +0300

    create RegisterForm

--- awardsapp/forms.py
+++ awardsapp/forms.py
@@ -3,6 +3,15 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile,Post
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
 
 class UpdateUserForm(forms.ModelForm):
     email = forms.EmailField(max_length=254, help_text='Required.Enter a valid email address. ')
