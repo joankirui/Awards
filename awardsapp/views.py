1ad96614be495ff5b7f2916cc3fc7cb88cc98ac6
joankirui <joankirui99@gmail.com>
2021-12-11 23:11:28 +0300

    update view to add the forms we just created

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .forms import UpdateUserForm,UpdateProfileForm
 
 # Create your views here.
 @login_required
 def profile(request):
     return render(request,'profile.html')
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users-profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+    args = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+    return render(request, 'edit.html', args)
