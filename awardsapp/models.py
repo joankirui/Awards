f8c8f05f5aff99c2a4afab68bd54d6bfeb0ee0c1
joankirui <joankirui99@gmail.com>
2021-12-11 21:08:33 +0300

    create model for posts

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -10,7 +10,18 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     prof_pic = CloudinaryField('image')
     bio = models.CharField(max_length=30,blank=True,null=True)
-    contact = models.EmailField()
+    contact = models.EmailField(max_length=100)
 
     def __str__(self):
         return f'{self.user.username}Profile'
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    photo = CloudinaryField('image')
+    description = models.TextField(max_length=255)
+    url = models.URLField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    user = models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='posts')
+
+    def __str__(self):
+        return f'{self.title}'
