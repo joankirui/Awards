5fc951597326a6226f8d979dcd0e7560b22b400f
joankirui <joankirui99@gmail.com>
2021-12-13 13:34:44 +0300

    create testcases for Profile model that pass

--- awardsapp/tests.py
+++ awardsapp/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Profile,Post,Rating
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='joan',password='weuh')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user,User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
